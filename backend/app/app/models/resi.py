2b2e0f270a6b36f5aa0315fd73373ab56903547c
williamchand <williamchans57@gmail.com>
2021-08-14 18:08:46 +0700

    data models

--- backend/app/app/models/resi.py
+++ backend/app/app/models/resi.py
@@ -1,16 +1,18 @@
 from typing import TYPE_CHECKING
-
 from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+import datetime
 
 from app.db.base_class import Base
 
 if TYPE_CHECKING:
-    from .item import Item  # noqa: F401
+    from .user import User  # noqa: F401
 
 
 class Resi(Base):
+    __tablename__ = 'resi'
     id = Column(Integer, primary_key=True, index=True)
     tracking_name = Column(String, index=True)
     driver_id = Column(Integer, ForeignKey("user.id"))
@@ -20,4 +22,29 @@ class Resi(Base):
     receiving_address = Column(String, nullable=True)
     sending_country = Column(String, nullable=True)
     receiving_country = Column(String, nullable=True)
-    driver = relationship("User", back_populates="resiList")
+    driver = relationship("User", back_populates="driver_resi")
+    resi_user = relationship("UserResi", back_populates="resi")
+
+class UserResi(Base):
+    __tablename__ = 'user_resi'
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    resi_id = Column(Integer, ForeignKey("resi.id"))
+    user = relationship("User", back_populates="user_resi")
+    resi = relationship("Resi", back_populates="resi_user")
+    user_resi_history = relationship("UserResiHistory", back_populates="user_resi")
+
+class StatusResi(Base):
+    __tablename__ = 'status_resi'
+    id = Column(Integer, primary_key=True, index=True)
+    status = Column(String, nullable=False)
+    
+class UserResiHistory(Base):
+    __tablename__ = 'user_resi_history'
+    id = Column(Integer, primary_key=True, index=True)
+    user_resi_id = Column(Integer, ForeignKey("user_resi.id"), nullable=False)
+    status_id = Column(Integer, ForeignKey("status.id"), nullable=False)
+    created_timestamp = Column(TIMESTAMP, default=datetime.datetime.utcnow, nullable=False)
+    user_resi = relationship("UserResi", back_populates="user_resi_history")
+    status = relationship("StatusResi")
+    
--- backend/app/app/models/user.py
+++ backend/app/app/models/user.py
@@ -2,10 +2,12 @@ from typing import TYPE_CHECKING
 
 from sqlalchemy import Boolean, Column, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
 
 from app.db.base_class import Base
 
 if TYPE_CHECKING:
+    from .resi import UserResi, Resi  # noqa: F401
     from .item import Item  # noqa: F401
 
 
@@ -16,4 +18,15 @@ class User(Base):
     hashed_password = Column(String, nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
+    user_photo = relationship("UserPhoto", back_populates="user")
     items = relationship("Item", back_populates="owner")
+    driver_resi = relationship("Resi", back_populates="driver")
+    user_resi = relationship("UserResi", back_populates="user")
+    
+
+class UserPhoto(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    photo = Column(String,  nullable=False)
+    user = relationship("User", back_populates="user_photo")
+    
